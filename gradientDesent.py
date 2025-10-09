216d068e9e4d5d024e6fe28350e5ad6fc9f65c37
trhoaq <hoangphi.wordcite.zaun@gmail.com>
2025-10-09 11:16:16 +0700

    update gradient descent

--- gradientDesent.py
+++ gradientDesent.py
@@ -3,34 +3,33 @@ import numpy as np
 import random as rand
 
 
-x = rand.randint(20, 50)
-learning_rate = 0.001
-y = lambda x: x**2
-dy = lambda x: 2*x
+# x = rand.randint(20, 50)
+x= 5
+learning_rate = 0.01
+y = lambda x: x**2 +5*np.sin(x)
+dy = lambda x: 2*x+5*np.cos(x)
 count = 0
 
 X1 = [x]
 Y1 = [y(x)]
 
 
-while x>0.05:
+while abs(dy(x)) > 1e-3 :
     count +=1
     x -= learning_rate*dy(x)
-    gradient_desent = y(x)
+    gradient_descent = y(x)
 
     X1.append(x)
-    Y1.append(gradient_desent)
+    Y1.append(gradient_descent)
 
-    if gradient_desent<0.05:
-        print(f"result: {gradient_desent}\nstep: {count}")
-        break
+print(f"Result: f(x) = {gradient_descent:.4f} tại x = {x:.4f}, bước = {count}")
 
 X2 = np.array(X1)
 Y2 = np.array(Y1)
-Count = np.array([x for x in range(0, count+1)])
+step = np.arange(len(Y2))
 
 plt.figure(figsize=(10, 6))
-plt.plot( Count, Y2, marker="o", label="f(x)", color="red" )
+plt.plot( step, Y2, marker="o", label="f(x)", color="red" )
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.title("Biểu diễn gradient desent theo x")
